eeefa74517bd60ba0ff150ea6432dca78f86b8a0
nastia kaminovska <kaminovska.nastia@gmail.com>
2025-06-03 02:43:16 +0300

    Remove unneeded reference

--- frontend/bookstore-crm/src/app/app.config.ts
+++ frontend/bookstore-crm/src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';  
 import { Box, LogIn, LucideAngularModule, Trash2, X }            from 'lucide-angular';
 import { Home, ShoppingCart, Package as packageIcon, BookOpen, Users, LogOut, ChevronLeft, ChevronRight } from 'lucide';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './core/interceptors/auth';
@@ -28,7 +27,6 @@ export const appConfig: ApplicationConfig = {
         LogIn
       })
     ),
-    importProvidersFrom(NgbModule),
     provideHttpClient(
       withInterceptorsFromDi()
     ),
