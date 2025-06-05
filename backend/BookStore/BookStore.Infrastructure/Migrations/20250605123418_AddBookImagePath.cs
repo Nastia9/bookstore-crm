96b192fbbe1038392360b090d30431508db66e7f
Kostya Bilomazur <k.bilomazur@lionwood.software>
2025-06-05 15:34:43 +0300

    add db migration

---
+++ backend/BookStore/BookStore.Infrastructure/Migrations/20250605123418_AddBookImagePath.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookImagePath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Books");
+        }
+    }
+}
